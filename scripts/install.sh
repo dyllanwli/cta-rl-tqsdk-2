363f6f5db45daf608c5abf541b83f3720e3491cd
dyllanwli <dyllanwli>
2022-11-13 13:21:22 -0600

    update slurm

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
 conda activate quant
 conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
-
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install -U tensorflow
+pip install pytorch-forecasting
+
 pip install gym
 pip install tqsdk
 pip install tqsdk2
@@ -12,8 +14,6 @@ pip install -U "ray[rllib]"  # installs Ray + dependencies for Ray RLlib
 pip install -U "ray[serve]"  # installs Ray + dependencies for Ray Serve
 pip install autokeras
 pip install -U modin
-pip install pytorch-forecasting
 pip install -q -U keras-tuner
-pip install -U tensorflow
 pip install wandb
 pip install pyyaml==6.0
