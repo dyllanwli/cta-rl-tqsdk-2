79c474480996289222572e0e0ceb9046ad37971f
dyllanwli <messsking@gmail.com>
2022-11-15 10:21:50 -0600

    update constant

--- src/utils/constant.py
+++ src/utils/constant.py
@@ -16,7 +16,7 @@ INTERVAL = Interval()
 low_by_label_length = {
     INTERVAL.FIVE_SEC: {
         5: 0.0001, # 0.01%
-        10: 0.0002, # 0.02%
+        10: 0.0005, # 0.05%
     }, 
     INTERVAL.ONE_MIN: {
         10: 0.0015, # 0.15%
@@ -27,7 +27,7 @@ low_by_label_length = {
 high_by_label_length = {
     INTERVAL.FIVE_SEC: {
         5: 0.0005, # 0.05% 0.02% is the minimum for this interval
-        10: 0.001, # 0.075%
+        10: 0.001, # 0.1%
     },
     INTERVAL.ONE_MIN: {
         10: 0.003, # 0.3%
