a14bf95092405921c18c17f0215ff18e5e8869b1
dyllanwli <messsking@gmail.com>
2022-12-28 21:25:37 -0600

    update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ pip install -U "ray[air]" # installs Ray + dependencies for Ray AI Runtime
 pip install -U "ray[tune]"  # installs Ray + dependencies for Ray Tune
 pip install -U "ray[rllib]"  # installs Ray + dependencies for Ray RLlib
 pip install -U "ray[serve]"  # installs Ray + dependencies for Ray Serve
-pip install autokeras
+# pip install autokeras
 pip install -U modin
 pip install -q -U keras-tuner
 pip install wandb
